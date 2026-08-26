59d74d214c87d5fb416916ff407e5c4a74933950
ddrabic-mis <info@udruga-mis.hr>
2026-08-26 13:38:32 +0200

    dan5_1

---
+++ dan_5/data/podaci.py
@@ -0,0 +1,34 @@
+osobe=[
+  {
+    'ime': 'Dejan',
+    'mjesto': 'Čakovec'
+  },
+  {
+    'ime': 'Lovro',
+    'mjesto': 'Čakovec'
+  },
+  {
+    'ime': 'Adam',
+    'mjesto': 'Zagreb'
+  },
+  {
+    'ime': 'Ante',
+    'mjesto': 'Zagreb'
+  },  {
+    'ime': 'Filip',
+    'mjesto': 'Zagreb'
+  },  {
+    'ime': 'Sergej',
+    'mjesto': 'Karlovac'
+  },
+]
+
+boje=[
+  "red",
+  "blue",
+  "green",
+  "yellow",
+  "orange",
+  "purple",
+  "pink",
+]
---
+++ dan_5/main.py
@@ -0,0 +1,28 @@
+# uključuejmo fastapi biblioteku i iz nje FastAPI i Request
+from fastapi import FastAPI, Request
+# uključuejmo Jinja2Templates iz fastapi biblioteke za rad s predlošcima
+from fastapi.templating import Jinja2Templates
+# uključuejmo Response iz fastapi.responses za vraćanje HTML odgovora
+from fastapi.responses import HTMLResponse
+
+from data.podaci import *
+import random
+
+app = FastAPI()
+templates = Jinja2Templates(directory="templates")
+
+@app.get("/", response_class=HTMLResponse)
+def pocetna(request: Request):
+    osoba = random.choice(osobe)
+    boja= random.choice(boje)
+    fsize=random.randint(24, 256)
+    return templates.TemplateResponse(
+        request=request,
+        name="index.html",
+        context={
+            'ime': osoba['ime'],
+            'mjesto': osoba['mjesto'],
+            'boja': boja,
+            'velicina': fsize
+            },
+        )
