3295df2827b927d1b98c561194ff814fb284badf
ddrabic-mis <info@udruga-mis.hr>
2026-08-27 13:51:31 +0200

    dan6_3

--- dan_6/main.py
+++ dan_6/main.py
@@ -3,31 +3,24 @@ from fastapi import FastAPI, Request, Response
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import RedirectResponse
 from fastapi.responses import HTMLResponse
+from starlette.middleware.sessions import SessionMiddleware
 
 app = FastAPI()
+app.add_middleware(SessionMiddleware, secret_key="tik-tak-toe-tajni-kljuc")
 templates = Jinja2Templates(directory="templates")
 
-igra_aktivna = True # igra je aktivna
-krizic=True
-pobjednik=None
-
-def init():
-  global igra_aktivna, krizic, pobjednik
+def init(request: Request):
   # početak igre
-  igra_aktivna = True
-  krizic=True
-  pobjednik=None
-  return [
+  request.session["igra_aktivna"] = True
+  request.session["krizic"] = True
+  request.session["pobjednik"] = None
+  request.session["polje"] = [
     [0, 0, 0],
     [0, 0, 0],
     [0, 0, 0]
   ]
 
-# pamtiti ćemo rezultat u memoriji - matrici 3x3
-polje=init() # postavili smo početnu matricu
-
-def odredi_pobjednika():
-  global polje
+def odredi_pobjednika(polje):
   # po redovima
   for i in range(3):
     redak="".join([str(x) for x in polje[i]])
@@ -53,38 +46,44 @@ def odredi_pobjednika():
 
 @app.get("/", response_class=HTMLResponse)
 def index(request: Request):
+    if "polje" not in request.session:
+       init(request)
     return templates.TemplateResponse(
        request=request,
        name="index.html",
        context={
-          "polje": polje, 
-          "aktivna": igra_aktivna,
-          "krizic": krizic,
-          "pobjednik": pobjednik
+          "polje": request.session["polje"],
+          "aktivna": request.session["igra_aktivna"],
+          "krizic": request.session["krizic"],
+          "pobjednik": request.session["pobjednik"]
           }
        )
 
 @app.post("/nova_igra", response_class=HTMLResponse)
 def nova_igra(request: Request):
-    global polje
-    polje=init() # postavili smo početnu matricu
+    init(request) # postavili smo početnu matricu
     # redirect na početnu stranicu
-    return RedirectResponse(url="/", status_code=303) 
+    return RedirectResponse(url="/", status_code=303)
 
 @app.get("/potez/{i}/{j}/{igrac_krizic}", response_class=HTMLResponse)
 def potez(request: Request, i: int, j: int, igrac_krizic: bool):
-    global polje, igra_aktivna, pobjednik, krizic
+    if "polje" not in request.session:
+        init(request)
+    polje = request.session["polje"]
+    igra_aktivna = request.session["igra_aktivna"]
     if not igra_aktivna:
-        return RedirectResponse(url="/", status_code=303) 
+        return RedirectResponse(url="/", status_code=303)
     if polje[i][j] != 0:
-        return RedirectResponse(url="/", status_code=303) 
+        return RedirectResponse(url="/", status_code=303)
     # odigrali smo potez
     polje[i][j] = 1 if igrac_krizic else 2
     # provjeriti je li netko pobijedio
-    pobjednik = odredi_pobjednika()
+    pobjednik = odredi_pobjednika(polje)
+    request.session["polje"] = polje
+    request.session["pobjednik"] = pobjednik
     if pobjednik:
-        igra_aktivna = False
+        request.session["igra_aktivna"] = False
     else:
         # promijeniti igrača
-        krizic = not krizic
+        request.session["krizic"] = not request.session["krizic"]
     return RedirectResponse(url="/", status_code=303)
